2602cd309f2f0aff7232f4a10f473cd387a4c68e
Maurice Sharpe <msharpe@e1z1r4p44.42.us.org>
2016-08-22 02:53:44 -0700

    My first upload

---
+++ main_alphabet.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_alphabet.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msharpe <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/08/22 01:40:42 by msharpe           #+#    #+#             */
+/*   Updated: 2016/08/22 02:50:06 by msharpe          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char ft_print_alphabet (char c);
+char ft_putchar (char c);
+char ft_print_reverse_alphabet (char c);
+char ft_print_numbers (char c);
+char ft_is_negative (char c);
+char ft_print_comb (char c);
+
+int	main(void)
+{
+	int a;
+
+	//a = '9';	
+
+	//ft_print_alphabet(a);
+	//ft_print_reverse_alphabet(a);
+	//ft_print_numbers(a);
+	//ft_is_negative(-a);
+	ft_print_comb(a);
+	ft_putchar('\n');
+	return (0);
+}
